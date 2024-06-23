733e22e64324271735c475aaae8f0f4e34bb9998
Virendra Patel <virendrapatel@koloapp.in>
2024-06-23 17:36:27 +0530

    Added loading

---
+++ app/src/app/meals/loading-out.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoadingMeals() {
+  return (
+    <div>
+      <h1>Loading Meals....</h1>
+      <p>Hold on please!</p>
+    </div>
+  );
+}
--- app/src/app/meals/page.jsx
+++ app/src/app/meals/page.jsx
@@ -1,18 +1,35 @@
 import MealList from "@/components/meal-list/meal-list";
-import React from "react";
+import React, { Suspense } from "react";
 
 import { fetchMeals } from "@/lib/meals";
+import styles from "./page.module.scss";
+import LoadingMeals from "./loading-out";
 
 export const metadata = {
   title: "Meals Listing page",
   description: "Some description for the page",
 };
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await fetchMeals();
+  return <MealList meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <div>
-      <MealList meals={meals} />
+      <div className={styles.content}>
+        <h1>Welcome to Our Burger Menu</h1>
+        <p>
+          Explore our delicious range of burgers, made with the freshest
+          ingredients and cooked to perfection. Whether you're a meat lover or a
+          veggie enthusiast, we've got something to satisfy your cravings.
+        </p>
+      </div>
+
+      <Suspense fallback={<LoadingMeals />}>
+        <Meals />
+      </Suspense>
     </div>
   );
 }
