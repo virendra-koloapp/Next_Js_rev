a0f580c176a1c944e59e1b85a5901a846d58ce2f
Virendra Patel <virendrapatel@koloapp.in>
2024-06-24 17:03:38 +0530

    Added not found handler

--- app/src/app/memes/[id]/page.jsx
+++ app/src/app/memes/[id]/page.jsx
@@ -2,13 +2,23 @@ import { getMemeById } from "@/lib/memes";
 import React from "react";
 import styles from "./page.module.scss";
 import { notFound } from "next/navigation";
+import { headers } from "next/headers";
+
+export async function generateMetadata({ params }) {
+  const meme = await getMemeById(params.id).catch(notFound);
+
+  if (!meme) {
+    return notFound();
+  }
+
+  return {
+    title: meme.name,
+  };
+}
 
 export default async function MemeDetails({ params }) {
   const id = params.id;
-
   const meme = await getMemeById(id);
-
-  console.log(meme, id);
 
   if (!meme) {
     return notFound();
